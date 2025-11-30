22450f282e6aa74c88842a128c62de5cd16d1929
Rogério Gotardo <120601204+RogerioGotardo@users.noreply.github.com>
2025-11-30 16:54:03 -0300

    Primeira versão do contador de cliques

---
+++ src/Contador.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Contador.css';
+
+const Contador = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementarContador = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="container">
+      <h1>Contador: {count}</h1>
+      <button onClick={incrementarContador}>Clique aqui</button>
+    </div>
+  );
+};
+
+export default Contador;
